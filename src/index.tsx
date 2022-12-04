a1b2ff8e2631d4af5bf06c16d56f64d59ec795ed
徐翔 <xuxiang001@zhongan.com>
2022-12-04 21:18:29 +0800

    docs: edit index

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@
  * @Author:
  * @Data: Do not edit
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2022-12-04 16:40:55
+ * @LastEditTime: 2022-12-04 21:17:52
  */
 import React from 'react'
 // import { createRoot } from 'react-dom/client'
@@ -22,5 +22,5 @@ import React from 'react'
 // const root = createRoot(app!)
 
 // root.render(<React.StrictMode>{rootNode()}</React.StrictMode>)
-const Header = () => <div>this is Header</div>
+const Header = () => <div>this is Header 1231231312312312</div>
 export default Header
